0c5f80c0f54fbc91cd5cae8f13832b34bf0a8834
satsura88 <satsura88@gmail.com>
2022-06-22 14:37:52 +0300

    пример val var | изменяемая и не изменяемая

--- app/src/main/java/com/example/kotlinlesson1/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson1/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var field1 = 5
+        val field2 = 5
+
+        field2 = 45
+        field1 = 21
     }
+}
+
+class Test constructor(val i: Int, var ){
+
 }
