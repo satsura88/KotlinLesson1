aa6f62e2c46f181e54a5d0a5b6a2dd7c8d26510e
satsura88 <satsura88@gmail.com>
2022-06-23 14:20:07 +0300

    пример get set

--- app/src/main/java/com/example/kotlinlesson1/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson1/MainActivity.kt
@@ -11,11 +11,20 @@ class MainActivity : AppCompatActivity() {
         var field1 = 5
         val field2 = 5
 
-        field2 = 45
+        //field2 = 45
         field1 = 21
+
+        val test1 = Test(1,2)
+        val test2 = Test(1,2)
+
+        //val newI = test1.getValI()
+        val newI2 = test1.valI
+        test1.varI = newI2
+        //test1.valI = newI2
+        //test1.serVarI(newI)
+
+
     }
 }
 
-class Test constructor(val i: Int, var ){
-
-}
+class Test constructor(val valI: Int, var varI: Int ){}
